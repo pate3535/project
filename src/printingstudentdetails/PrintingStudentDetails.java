cf45f03375c546b284d1559507b0f2e102d8f376
pate3535 <pate3535@sheridancollege.ca>
2022-05-19 11:37:17 -0400

    version 1 of student project

---
+++ src/printingstudentdetails/PrintingStudentDetails.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printingstudentdetails;
+
+/**
+ *
+ * @author aayus
+ */
+public class PrintingStudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    Student s1= new Student();
+    Student[] list= new Student[3];
+    
+    
+    
+    s1.setName("john");
+    s1.setAge(19);
+    s2.setName("peter");
+    s2.setAge(19);
+    s3.setName("Harman");
+    s3.setAge(19);
+    list[0]=s1;
+    list[1]=s2;
+    list[2]=s3;
+    
+    for(i=o;)
+    
+    s.out)list[i]get name and getage 
+    
+    
+}
