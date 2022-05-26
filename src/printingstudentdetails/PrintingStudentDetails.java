24ca3d9e84d3da2b1b170fcd4ec6c4251608d448
pate3535 <pate3535@sheridancollege.ca>
2022-05-26 09:32:52 -0400

    week 3(add name)

--- src/printingstudentdetails/PrintingStudentDetails.java
+++ src/printingstudentdetails/PrintingStudentDetails.java
@@ -10,30 +10,35 @@ package printingstudentdetails;
  */
 public class PrintingStudentDetails {
 
-    /**
-     * @param args the command line arguments
+    /*
+     * Aayushi patel
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    Student s1= new Student();
+   
     Student[] list= new Student[3];
     
     
     
+     Student s1= new Student();
     s1.setName("john");
     s1.setAge(19);
+    Student s2= new Student();
     s2.setName("peter");
     s2.setAge(19);
+     Student s3= new Student();
     s3.setName("Harman");
     s3.setAge(19);
     list[0]=s1;
     list[1]=s2;
     list[2]=s3;
     
-    for(i=o;)
+    for(int i=o;i<list.length;i++){
     
-    s.out)list[i]get name and getage 
+}
+    
+    System.out.println(list[i].getName()+"" +list
     
     
 }
